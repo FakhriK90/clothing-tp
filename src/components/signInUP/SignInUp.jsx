84d424060c7944988a726f61c9ab0b2d299270ab
FakhriK90 <eza3im2009@live.fr>
2024-10-22 15:41:43 +0200

    establishing sign in with password and emil using firebase authentification

--- src/components/signInUP/SignInUp.jsx
+++ src/components/signInUP/SignInUp.jsx
@@ -1,20 +1,100 @@
 import "./signInUp.styles.scss";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { createUserDocumentFromAuth, signInWithGoogleAPopup,  } from "../../utils/firebase/Firebase.utils";
+import {
+  createUserDocumentFromAuth,
+  signInWithGoogleAPopup,
+  createAuthUserWithEmailAndPassword,
+  signInWithEmailPassword,
+} from "../../utils/firebase/Firebase.utils";
+
+const defaultFormFields = {
+  fullName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const SignInUp = () => {
-    const [signIn, setSignIn] = useState(true);
+  /* Creation of user*/
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { fullName, email, password, confirmPassword } = formFields;
 
-    const toggle = () => {
-      setSignIn(!signIn);
-    };
+  console.log(formFields);
+  const resetFields = () => {
+    setFormFields(defaultFormFields);
+  };
+  /* Sign up with email and password*/
 
-    const logGoogleUser = async () => {
-      const {user} = await signInWithGoogleAPopup();
-      // 
-    const userDocRef = await  createUserDocumentFromAuth(user);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Password don't match");
+      return;
     }
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserDocumentFromAuth(user, { fullName });
+      resetFields();
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("Email already in use");
+      } else {
+        console.log("OOps! user didn't created :(", error.message);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  /* Google sign in*/
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGoogleAPopup();
+    //
+    await createUserDocumentFromAuth(user);
+  };
+
+  /* Sign in with email and password */
+
+  // const handleChangeSignIn = (event) => {
+  //   const { name, value } = event.target;
+  //   if (name === 'email') {
+  //     setFormFields(value);
+  //   } else if (name === 'password') {
+  //     setFormFields(value);
+  //   }
+  // };
+
+  const handleSubmitSignIn = async (event) => {
+    event.preventDefault();
+    try {
+      await signInWithEmailPassword(email, password);
+      resetFields();
+      alert("Sign in successful!");
+    } catch (error) {
+      if (error.code === "auth/user-not-found") {
+        console.log("User not found. Please check your email and password.");
+      } else if (error.code === "auth/wrong-password") {
+        console.log("Incorrect password. Please try again.");
+      } else {
+        console.log("An error occurred during sign-in:", error.message);
+      }
+    }
+  };
+
+  /* Toggle from sign in to sign up*/
+  const [signIn, setSignIn] = useState(true);
+
+  const toggle = () => {
+    setSignIn(!signIn);
+  };
+
   return (
     <div
       id="container"
@@ -25,14 +105,35 @@ const SignInUp = () => {
           <div className="col align-items-center flex-col sign-in">
             <div className="form-wrapper align-items-center">
               <div className="login-box" id="signInUp">
-                <form action="">
+                <form action="" onSubmit={handleSubmit}>
                   <h2>Sign Up</h2>
+                  <div className="input-box">
+                    <span className="icon">
+                      <ion-icon name="person"></ion-icon>
+                    </span>
+                    <div className="input-wrapper">
+                      <input
+                        type="text"
+                        name="fullName"
+                        value={fullName}
+                        required
+                        onChange={handleChange}
+                      />
+                      <label htmlFor="text">Full Name</label>
+                    </div>
+                  </div>
                   <div className="input-box">
                     <span className="icon">
                       <ion-icon name="mail"></ion-icon>
                     </span>
                     <div className="input-wrapper">
-                      <input type="email" required />
+                      <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        required
+                        onChange={handleChange}
+                      />
                       <label htmlFor="email">Email</label>
                     </div>
                   </div>
@@ -41,13 +142,34 @@ const SignInUp = () => {
                       <ion-icon name="lock-closed"></ion-icon>
                     </span>
                     <div className="input-wrapper">
-                      <input type="password" required />
+                      <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        required
+                        onChange={handleChange}
+                      />
                       <label htmlFor="password">Password</label>
                     </div>
                   </div>
+                  <div className="input-box">
+                    <span className="icon">
+                      <ion-icon name="lock-closed"></ion-icon>
+                    </span>
+                    <div className="input-wrapper">
+                      <input
+                        type="password"
+                        name="confirmPassword"
+                        value={confirmPassword}
+                        required
+                        onChange={handleChange}
+                      />
+                      <label htmlFor="password">Confirm password</label>
+                    </div>
+                  </div>
                   <div className="btn-submit">
-                  <Link to="/">
-                    <button type="submit">Sign up</button>
+                    <Link to="/">
+                      <button type="submit">Sign up</button>
                     </Link>
                     <div className="register-link">
                       <span>Already have an account? </span>
@@ -64,14 +186,22 @@ const SignInUp = () => {
           <div className="col align-items-center flex-col sign-up">
             <div className="form-wrapper align-items-center">
               <div className="login-box">
-                <form action="">
+                <form action="" 
+                onSubmit={handleSubmitSignIn}
+                >
                   <h2>Sign In</h2>
                   <div className="input-box">
                     <span className="icon">
                       <ion-icon name="mail"></ion-icon>
                     </span>
                     <div className="input-wrapper">
-                      <input type="email" required />
+                      <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        required
+                        onChange={handleChange}
+                      />
                       <label htmlFor="email">Email</label>
                     </div>
                   </div>
@@ -80,7 +210,13 @@ const SignInUp = () => {
                       <ion-icon name="lock-closed"></ion-icon>
                     </span>
                     <div className="input-wrapper">
-                      <input type="password" required />
+                      <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        required
+                        onChange={handleChange}
+                      />
                       <label htmlFor="password">Password</label>
                     </div>
                   </div>
@@ -93,25 +229,38 @@ const SignInUp = () => {
                       </b>
                     </div>
                   </div>
-                      <div className="social-account-container">
-        <span className="title">Or Sign in with</span>
-        <div className="social-accounts">
-          <button onClick={logGoogleUser} className="social-button google" title="Sign in with Google">
-            <svg className="svg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 488 512">
-              <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
-            </svg></button>
-          <button className="social-button apple" title="Sign in with Apple">
-            <svg className="svg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
-              <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path>
-            </svg>
-          </button>
-          <button className="social-button twitter" title="Sign in with Twitter">
-            <svg className="svg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-              <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
-            </svg>
-          </button>
-        </div>
-      </div>
+                  <div className="social-account-container">
+                    <span className="title">Or Sign in with</span>
+                    <div className="social-accounts">
+                      <button
+                        onClick={logGoogleUser}
+                        className="social-button google"
+                        title="Sign in with Google"
+                      >
+                        <svg
+                          className="svg"
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="1em"
+                          viewBox="0 0 488 512"
+                        >
+                          <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
+                        </svg>
+                      </button>
+                      <button
+                        className="social-button apple"
+                        title="Sign in with Apple"
+                      >
+                        <svg
+                          className="svg"
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="1em"
+                          viewBox="0 0 384 512"
+                        >
+                          <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
                 </form>
               </div>
             </div>
@@ -134,7 +283,7 @@ const SignInUp = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignInUp
+export default SignInUp;
--- src/utils/firebase/Firebase.utils.jsx
+++ src/utils/firebase/Firebase.utils.jsx
@@ -4,13 +4,10 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from "firebase/auth";
-import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc,
-} from "firebase/firestore";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCZaoxaNq1R_W93ErF4mM1TjIIeLiGA9NA",
@@ -21,35 +18,71 @@ const firebaseConfig = {
   appId: "1:35933860908:web:7ea64d86e7e6cc18428d89",
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googlProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
-    prompt:"select_account"
-})
+googlProvider.setCustomParameters({
+  prompt: "select_account",
+});
 
 export const auth = getAuth();
-export const signInWithGoogleAPopup = () => signInWithPopup(auth, provider);
+
+export const signInWithGoogleAPopup = () =>
+  signInWithPopup(auth, googlProvider);
+
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googlProvider);
+
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {
+
+}) => {
+  if (!userAuth) {
+    return;
+  }
   const userDocRef = doc(db, "users", userAuth.uid);
-  
+
   const userSnapshot = await getDoc(userDocRef);
 
   if (!userSnapshot.exists()) {
-    const { displayName, email } = userAuth;
+    const { fullName, email } = userAuth;
     const createdAt = new Date();
     try {
       await setDoc(userDocRef, {
-        displayName,
+        fullName,
         email,
         createdAt,
+        ...additionalInformation
       });
     } catch (error) {
       console.log("Error creating user", error.message);
     }
   }
   return userDocRef;
+};
+
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) {
+    return;
+  }
+
+  return await createUserWithEmailAndPassword(auth, email, password);
 }
+
+/* Sign in with email and password */
+
+export const signInWithEmailPassword = async (email, password) => {
+  if (!email || !password) {
+    return;
+  }
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error('Error signing in with email and password', error);
+  }
+};
