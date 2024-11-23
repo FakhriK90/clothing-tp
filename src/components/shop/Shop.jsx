a7c4270d44d56def9168e5466cc0f9cb2d62ba41
FakhriK90 <eza3im2009@live.fr>
2024-11-23 15:17:42 +0100

    Using the shop-item component

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,21 +1,17 @@
 import { useContext } from 'react';
 import {ProductsContext} from "../../contexts/products.context";
+import ShopItem from '../shop-item/ShopItem';
+import './shop.styles.scss';
 
 const Shop = () => {
     const {products} = useContext(ProductsContext);
     return (
         <div className="shop-page">
-            {products.map(({ id, title, items }) => (
-                <div key={id} className="collection-preview">
-                    <h1 className="title">{title.toUpperCase()}</h1>
-                    <div className="preview">
-                        {items
-                            .filter((item, idx) => idx < 4)
-                            .map((item) => (
-                                <div key={item.id}>{item.name}</div>
-                            ))}
-                    </div>
-                </div>
+            {products.map(category => (
+                category.items.map(item => (
+                    console.log(item),
+                    <ShopItem key={item.id} item={item} />
+                ))
             ))}
         </div>
     );
