9092244cca65e5956ba3592340d4a77124dd7aeb
FakhriK90 <eza3im2009@live.fr>
2024-11-23 09:59:30 +0100

    Updating main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import App from './App.jsx'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context.jsx';
+import { ProductsProvider } from './contexts/products.context.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <UserProvider>
+  <ProductsProvider>
     <StrictMode>
     <App />
   </StrictMode>
+  </ProductsProvider>
   </UserProvider>
   </BrowserRouter>
-  
 )
