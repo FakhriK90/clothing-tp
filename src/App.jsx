b536725b4a8bc74921703792866b8b8cf3f7c64d
FakhriK90 <eza3im2009@live.fr>
2024-11-23 09:33:30 +0100

    removing products from app.jsx to keep code clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.scss";
+import categories from "./Products-data.js";
 import Categories from "./components/categories/Categories";
 import Navigation from "./components/navigation/Navigation";
 import Footer from "./components/footer/Footer";
@@ -8,33 +9,7 @@ import SignUp from "./components/signInUP/SignUp";
 
 function App() {
   const location = useLocation();
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Women",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Men",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+
 
   return (
     <>
