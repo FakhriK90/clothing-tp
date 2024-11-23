41425f9b92af91fddb828973b4f29a44438c7e4f
FakhriK90 <eza3im2009@live.fr>
2024-11-23 10:14:00 +0100

    Setting our products context

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,21 @@
+import {createContext, useState} from "react";
+import PropTypes from 'prop-types';
+import PRODUCTS from "../shop-data.json";
+
+export const ProductsContext = createContext({
+    products: [],
+});
+
+export const ProductsProvider = ({children}) => {
+    const [products, setProducts] = useState(PRODUCTS);
+    const value = {products}
+    return (
+        <ProductsContext.Provider value={value}>
+            {children}
+        </ProductsContext.Provider>
+    );
+};
+
+ProductsProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
