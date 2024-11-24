af8855332d12a5f47fb116eed0adbcc8273ec58c
FakhriK90 <eza3im2009@live.fr>
2024-11-24 09:53:24 +0100

    Using the toogle function to state our cart dropdown when it's open or not

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,10 +1,15 @@
+import {useContext} from 'react';
+import {CartContext} from '../../contexts/cart.context';
 import './cart-icon.styles.scss';
 import ShoppingBag from './ShoppingBag';
 
 
 const CartIcon = () => {
+  const {isCartOpen, setIsCartOpen} = useContext(CartContext);
+
+  const toogleIsCartOpen = ()=> setIsCartOpen(!isCartOpen);
   return (
-    <div className='cart-icon-container'>
+    <div className='cart-icon-container' onClick={toogleIsCartOpen}>
         <ShoppingBag className='shopping-icon'/>
         <span className='item-count'>0</span>
     </div>
