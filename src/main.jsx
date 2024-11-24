3624d33d6dea50797e53a7e02403eec023aaf503
FakhriK90 <eza3im2009@live.fr>
2024-11-24 10:01:21 +0100

    Updating our main.jsx with the cart provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,17 @@ import './index.scss'
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context.jsx';
 import { ProductsProvider } from './contexts/products.context.jsx';
-import {CartContext} from './contexts/cart.context.jsx'
+import {CartProvider} from './contexts/cart.context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <UserProvider>
   <ProductsProvider>
-  <CartContext>
+  <CartProvider>
     <StrictMode>
     <App />
   </StrictMode>
-  </CartContext>
+  </CartProvider>
   </ProductsProvider>
   </UserProvider>
   </BrowserRouter>
