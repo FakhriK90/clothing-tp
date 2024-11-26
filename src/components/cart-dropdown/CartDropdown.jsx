404db33bbaff7caeca3451f8b1866d0831fd9d39
FakhriK90 <eza3im2009@live.fr>
2024-11-26 20:45:06 +0100

    Using the useNavigate for the button to navigate to the checkout page

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,11 +1,23 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CartContext } from '../../contexts/cart.context'
 import { Button } from '@mui/material';
 import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/CartItem';
 
 const CartDropdown = () => {
+  const {cartItems} = useContext(CartContext);
+  const navigate = useNavigate();
+
+    const handleCheckout = () => {
+        navigate('/checkout');
+    }
   return (
     <div className='cart-dropdown-container'>
-            <div className='cart-items'></div>
-            <Button className='button3'>GO TO CHECKOUT</Button>
+            <div className='cart-items'>
+              {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
+            </div>
+            <Button className='button3' onClick={handleCheckout} >GO TO CHECKOUT</Button>
         </div>
   )
 }
